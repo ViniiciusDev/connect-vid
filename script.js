b446a90a23882e74a7220114850d892c7eb13118
Vinicius Gomes <ViniiciusGomes27@gmail.com>
2024-06-10 21:51:19 +0200

    Async/Await

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const videosContainer = document.querySelector(".videos__container");
-const api = fetch("http://localhost:3000/videos")
-    .then((response) => response.json())
-    .then((videos) => {
+
+async function buscarElementosVideos() {
+    try {
+        const buscar = await fetch("http://localhost:3000/videos");
+        const videos = await buscar.json();
         videos.forEach((video) => {
+            if (video.categoria == "") {
+                throw new Error("Categoria não pode ser vazia");
+            }
             videosContainer.innerHTML += `
                 <li class="videos__item">
                     <iframe src=${video.url} title=${video.titulo} frameborder="0" allowfullscreen></iframe>
@@ -14,9 +19,11 @@ const api = fetch("http://localhost:3000/videos")
                 </li>
             `;
         });
-    })
-    .catch((error) => {
+    } catch (error) {
         videosContainer.innerHTML = `
-            <p> Houve um erro ao carregar os vídeos ${error} </p>
-        `;
-    });
+        <p> Houve um erro ao carregar os vídeos ${error} </p>
+    `;
+    }
+}
+
+buscarElementosVideos();
