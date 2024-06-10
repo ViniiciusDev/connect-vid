dda741d1229c5ce0200538ab510b478f9080ab78
Vinicius Gomes <ViniiciusGomes27@gmail.com>
2024-06-10 20:28:45 +0200

    Creazione JSON e Richiesta fetch

---
+++ script.js
@@ -0,0 +1,22 @@
+const videosContainer = document.querySelector(".videos__container");
+const api = fetch("http://localhost:3000/videos")
+    .then((response) => response.json())
+    .then((videos) => {
+        videos.forEach((video) => {
+            videosContainer.innerHTML += `
+                <li class="videos__item">
+                    <iframe src=${video.url} title=${video.titulo} frameborder="0" allowfullscreen></iframe>
+                    <div class="descricao-video">
+                        <img class="img-canal" src=${video.imagem} alt=${video.titulo}/>
+                        <h3 class="titulo-video">${video.titulo}</h3>
+                        <p class="titulo-canal">${video.descricao}</p>
+                    </div>
+                </li>
+            `;
+        });
+    })
+    .catch((error) => {
+        videosContainer.innerHTML = `
+            <p> Houve um erro ao carregar os vídeos ${error} </p>
+        `;
+    });
