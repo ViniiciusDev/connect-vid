e62808ce5b1d92dd8287b8d185bdf972d1369286
Vinicius Gomes <ViniiciusGomes27@gmail.com>
2024-06-18 21:56:04 +0200

    Filtro por Categoria e Barra de Pesquisa

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ async function buscarElementosVideos() {
                         <img class="img-canal" src=${video.imagem} alt=${video.titulo}/>
                         <h3 class="titulo-video">${video.titulo}</h3>
                         <p class="titulo-canal">${video.descricao}</p>
+                        <p class="categoria" hidden> ${video.categoria}</p>
                     </div>
                 </li>
             `;
@@ -25,5 +26,49 @@ async function buscarElementosVideos() {
     `;
     }
 }
-
 buscarElementosVideos();
+
+// Busca pelo input de pesquisa
+const searchBar = document.querySelector(".pesquisar__input");
+
+function buscarElementos() {
+    const videos = document.querySelectorAll(".videos__item");
+
+    videos.forEach((video) => {
+        const titulo = video
+            .querySelector(".titulo-video")
+            .textContent.toLowerCase();
+        const valorBarraDePesquisa = searchBar.value.toLowerCase();
+
+        if (titulo.includes(valorBarraDePesquisa)) {
+            video.style.display = "";
+        } else {
+            video.style.display = "none";
+        }
+    });
+}
+searchBar.addEventListener("input", buscarElementos);
+
+// Busca pela barra de categorias
+const botaoCategoria = document.querySelectorAll(".superior__item");
+
+botaoCategoria.forEach((botao) => {
+    const nomeCategoria = botao.getAttribute("name");
+    botao.addEventListener("click", () => buscaPorCategoria(nomeCategoria));
+});
+
+function buscaPorCategoria(filtro) {
+    const videos = document.querySelectorAll(".videos__item");
+    for (let video of videos) {
+        let categoria = video
+            .querySelector(".categoria")
+            .textContent.toLowerCase();
+        let valorFiltro = filtro.toLowerCase();
+
+        if (!categoria.includes(valorFiltro) && valorFiltro != "tudo") {
+            video.style.display = "none";
+        } else {
+            video.style.display = "block";
+        }
+    }
+}
